3608589f60c7ab1b1d5a4d08795a7db481c9e1f5
kellyvasss <99kelvas@gafe.molndal.se>
2023-05-10 14:44:46 +0200

    påbörjat alpha api klass

--- src/main/java/api/AlphaVantage.java
+++ src/main/java/api/AlphaVantage.java
@@ -1,4 +1,23 @@
 package api;
 
+
 public class AlphaVantage {
+    private String key = "";
+    private OkHttpClient client;
+    //private String function = "";
+    private String base_url = "https://www.alphavantage.co/query?function=";
+    // Ta in keyReader och kalla på metod för att få fram nyckel
+    public AlphaVantage(String key) {
+        this.key = key;
+        client = new OkHttpClient();
+    }
+
+    public static JSONObject companyOverview(String symbol) {
+        // Denna kommer låta användaren söka på en ticket och den genererar bolagsbeskrivning, fullständigt namn,
+        // sektor, land, symbol, targetprice, genomsnitt för 50 och 200 MA (moving average), utdelningsdag,
+        String function = "OVERVIEW";
+        String url = base_url + function +
+    }
+
+
 }
