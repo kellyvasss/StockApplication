fcd6d062c711bc4aeb86043ace7d3eaf5dbc7c3b
kellyvasss <99kelvas@gafe.molndal.se>
2023-05-19 00:23:16 +0200

    -

--- src/main/java/com/example/stockapp/HelloController.java
+++ src/main/java/com/example/stockapp/HelloController.java
@@ -5,9 +5,12 @@ import SQL.SQLite;
 import api.AlphaVantage;
 import api.KeyReader;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
 import stock.Stock;
+import user.NumberValidator;
 import user.hash;
 
 public class HelloController {
@@ -15,41 +18,23 @@ public class HelloController {
     private TextArea result;
     @FXML
     private Label welcomeText;
+    private TextInputDialog inputDialog;
     private KeyReader keyReader;
     private AlphaVantage alphaVantage;
-    private MySQL mySQL;
     private SQLite sqLite;
+
     public HelloController() {
-      //  keyReader = new KeyReader("Alpha");
-       // alphaVantage = new AlphaVantage(keyReader.getAPIKey());
-       // mySQL = new MySQL("root", keyReader.getKey());
-      /*  sqLite = new SQLite("hej");
-        sqLite.createTableUser();
-        sqLite.createTablePortfolio();
-        sqLite.createTableCountry();
-        sqLite.createTableDimCurrency();
-        sqLite.createTableDimIndustry();
-        sqLite.createTableDimMarket();
-        sqLite.createTableDimSector();
-        sqLite.createTableDimStock();
-        sqLite.createTableFactStockPrice();
-        sqLite.createTableFactTransaction();
-        String pas = hash.hasha("Kelly");
-        result = new TextArea();
-        result.setText(pas);
-
-*/
-
+        inputDialog = new TextInputDialog();
+        Button login = new Button("Logga in");
+        login.setOnAction(event -> onHelloButtonClick());
     }
+
 
     @FXML
     protected void onHelloButtonClick() {
 
-
         welcomeText.setText("Welcome to JavaFX Application!");
         result.setText(hash.hasha("oooooooooooooooooooooooooooooooooooooooooo"));
-
-       // result.setText(alphaVantage.searchEndpoint("apple").toString());
 
     }
 }
